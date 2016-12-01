0130fbb15f276910505eb7fb9e95ec36aea3056e
ewelina <skibinska.ewelina@gmail.com>
2016-12-01 17:14:59 +0000

    Move filter button

--- public/helpers/filter-link.js
+++ public/helpers/filter-link.js
@@ -1,6 +1,6 @@
 module.exports = (isFiltered) => {
   if (isFiltered) {
-    return '<a href="/resources" class="btn style-a">All resources</a>'
+    return '<a href="/resources" class="btn btn-filter style-a">All resources</a>'
   }
-  return '<a href="/resources?reviewed=true" class="btn style-a">Filter by reviewed resources</a>';
+  return '<a href="/resources?reviewed=true" class="btn btn-filter style-a">Filter by reviewed resources</a>';
 };
