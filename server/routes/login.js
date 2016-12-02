c0bcf393ebb8ecf3a196430d4fe637b6a5cf7f21
Cleop <cleo.pearson@hotmail.co.uk>
2016-12-02 12:27:03 +0000

    Modularises remaining route paths into the routes folder

---
+++ server/routes/login.js
@@ -0,0 +1,35 @@
+const login = require('../database/queries/login');
+
+module.exports = [
+  {
+    method: 'GET',
+    path: '/login',
+    handler: (req, reply) => {
+      reply.view('login');
+    }
+  },
+  {
+    method: 'POST',
+    path: '/login',
+    config: {
+      handler: (req, reply) => {
+        login([req.payload.email, req.payload.password], (error, result) => {
+          if (error) return reply(error).statusCode(400);
+          if (result.length === 0) {
+            return reply('User not found.');
+          }
+          req.cookieAuth.set(result[0]);
+          reply.redirect('reviews');
+        });
+      }
+    }
+  },
+  {
+    method: 'GET',
+    path: '/logout',
+    handler: (req, reply) => {
+      req.cookieAuth.clear();
+      return reply.redirect('/');
+    }
+  }
+];
