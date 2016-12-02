c84a8f92f1b3e170386bcce35f067e3ef016254f
ewelina <skibinska.ewelina@gmail.com>
2016-12-02 12:17:31 +0000

    Fix hover on links

--- public/helpers/get_reviews_large.js
+++ public/helpers/get_reviews_large.js
@@ -1,3 +1,3 @@
 module.exports = function(userReview){
-  return `<a class="style-a" href=resources/${userReview.resource_id}><li class="style-link divider">${userReview.title}   ${userReview.rating}/5 <br>${userReview.content}</li></a><button class="btn">Edit</button><button class="btn">Delete</button>`;
+  return `<a class="style-a" href=resources/${userReview.resource_id}><li class="style-link link-hover divider">${userReview.title}   ${userReview.rating}/5 <br>${userReview.content}</li></a><button class="btn">Edit</button><button class="btn">Delete</button>`;
 };
